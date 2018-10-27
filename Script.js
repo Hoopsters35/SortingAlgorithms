38679ac08f6d6b9b2a935a1967b015cfb9bc7ced
NullERR0R404 <jloudenslager24@gmail.com>
2018-10-27 10:30:33 -0400

    quicksort probably

--- Script.js
+++ Script.js
@@ -7,7 +7,32 @@
 // RADIX sort (3 types)
 
 // Quick sort
+var quickSort = function quickSort(array, low, high) {
+    if (low < high) {
+        var alpha = partition(array, low, high);
 
+        quickSort(array, low, alpha - 1);
+        quickSort(array, alpha + 1, high);
+    }
+}
+
+function partition(array, low, high) {
+    var pivot = array[high];
+    i = low - 1;
+
+    for(j = low; j < high; j++) {
+        if(array[j] <= pivot) {
+            i++
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+    var temp = array[i+1];
+    array[i + 1] = array[pivot];
+    array[pivot] = temp;
+    return (i + 1);
+}
 // Heap sort
 
 // BOGO sort
