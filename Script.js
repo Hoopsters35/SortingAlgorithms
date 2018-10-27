9cb5a17d7e3a6032296484d2b96cb9306374e111
Justin Knueppel <justinknueppel@gmail.com>
2018-10-27 10:02:29 -0400

    add comments for algorithms

---
+++ Script.js
@@ -0,0 +1,21 @@
+//Main script
+
+// Bubble sort
+
+// Insertion sort
+
+// RADIX sort (3 types)
+
+// Quick sort
+
+// Heap sort
+
+// BOGO sort
+
+// Shell sort
+
+// Merge sort
+
+// Counting sort
+
+// Bucket sort
