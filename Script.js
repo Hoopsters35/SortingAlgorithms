aa7e8054b2f56a2aaae3adfa434e993adef62de7
Justin Knueppel <justinknueppel@gmail.com>
2018-10-27 10:38:12 -0400

    Work on main script

--- Script.js
+++ Script.js
@@ -1,14 +1,37 @@
 //Main script
+var ARR_SIZE = 100;
+
 document.getElementById("btnSort").addEventListener("click",
     () => {
-        startSort(sortArrs)
+        let arr = getRandArr();
+        console.log(arr);
+        //startSort(arr);
     });
 startSort = (arr) => {
-    for (fn of sorters) {
+    for (fn of sortArrs) {
         fn(arr);
     }
 }
-var sortArrs = [bubbleSort];
+//var sortArrs = [bubbleSort];
+getRandArr = function() {
+    let arr = []
+    for (let i = 0; i < ARR_SIZE; i++) {
+        arr.push(i);
+    }
+    shuffle(arr);
+    return arr;
+}
+
+var shuffle = function(arr) {
+    let curIndex = arr.length;
+    while (curIndex > 0) {
+        let newIndex = Math.floor(Math.random() * curIndex);
+        let temp = arr[curIndex];
+        arr[curIndex] = arr[newIndex];
+        arr[newIndex] = temp;
+        curIndex--;
+    }
+}
 
 // Bubble sort
 
